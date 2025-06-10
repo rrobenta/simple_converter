0c80f3357fe11c90c1858c53ddb28f987c0c6bb4
roderick robenta <robentaroderick@gmail.com>
2025-06-10 23:57:01 +0800

    added convert function and connect it to the main file (gui.py)

--- gui.py
+++ gui.py
@@ -1,14 +1,22 @@
 import FreeSimpleGUI as sg
+from converters import convert
 
 label1 = sg.Text('Enter feet:')
-input1 = sg.InputText()
+input1 = sg.InputText(key="feet")
 
 label2 = sg.Text('Enter inches:')
-input2 = sg.InputText()
+input2 = sg.InputText(key="inches")
 
 btn = sg.Button('Convert')
+output_label = sg.Text("", key="output")
 
-window = sg.Window('Converter', layout = [[label1, input1],[label2, input2],[btn]])
+window = sg.Window('Converter', layout = [[label1, input1],[label2, input2],[btn, output_label]])
 
-window.read()
+while True:
+    event, values = window.read()
+    feet = float(values["feet"])
+    inches = float(values['inches'])
+    result = convert(feet, inches)
+    window["output"].update(value=f"{result} m", text_color='white')
+
 window.close()
